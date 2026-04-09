f1f18f8df3c336606f93ac4aa04a78687fba25fd
azamgohar <144214140+azamgohar@users.noreply.github.com>
2026-04-09 21:19:10 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let a = 2; 
+{
+    let b = 12;
+    {
+        let b = 4;
+        console.log(b);
+    }
+}
+
+function azam () {
+    {
+        let myName = "Azam Gohar";
+        var naam = "Azam Gohar";
+    }
+    console.log(naam);
+}
+
+azam()
+
+console.log(a);
